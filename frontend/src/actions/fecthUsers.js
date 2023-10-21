c57551e0d8cb59f95517e4f9f4483ec9eb58074f
Ingrid Fuentes <49660565+IngridFuentes@users.noreply.github.com>
2023-10-21 17:23:21 -0400

    Delete frontend directory

--- frontend/src/actions/fecthUsers.js
+++
@@ -1,9 +0,0 @@
-export const fetchUsers = () =>  {
-    return(dispatch) => {
-        dispatch({type: 'LOADING_USERS'});
-        fetch('http://localhost:3000/users')
-        .then(resp => {return resp.json()})
-        .then(users => { dispatch({ type: 'ADD_USER', payload: users})} );
-    };
-}
-
