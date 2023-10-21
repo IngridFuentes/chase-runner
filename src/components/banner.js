55b5f9336f37843ab012cefe895264e351050f21
hannavas2022 <112612098+hannavas2022@users.noreply.github.com>
2023-10-21 13:21:23 -0500

    web responsive design for web version

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,47 +1,41 @@
-import styles from '../styles/Banner.module.css';
-import Button from 'react-bootstrap/Button';
-<<<<<<< HEAD
-import Dropdown from 'react-bootstrap/Dropdown';
-=======
->>>>>>> 4ed30e5721e21fa7f0ba6b89df04b0f07f401fe3
+import styles from "../styles/Banner.module.css";
+import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function Banner() {
-    return ( 
-        <div className={styles.mainHeader}>
-            <div className={styles.projectLogo}></div>
-            <div className={styles.headerText}>CHASE RUNNER</div>
-            <div className={styles.spacer}></div>
-<<<<<<< HEAD
-            <Button variant="light">
-                <a href="https://passage.id/">STATS</a>
-            </Button>
-            <Button variant="light">
-                <a href="https://passage.id/">NEW RUN</a>
-            </Button>
-            <Button variant="light">
-                <a href="https://passage.id/">GOALS</a>
-            </Button>
-            <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic" style={{borderStyle:'none'}}>
-                <div className={styles.imageProfile}></div>
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                <Dropdown.Item href="/logout">Logout</Dropdown.Item>
-            </Dropdown.Menu>
-            </Dropdown>
-=======
-            <Button variant="dark">
-                <a href="https://passage.id/">STATS</a>
-            </Button>
-            <Button variant="dark">
-                <a href="https://passage.id/">NEW RUN</a>
-            </Button>
-            <Button variant="dark">
-                <a href="https://passage.id/">GOALS</a>
-            </Button>
-            <div className={styles.imageProfile}></div>
->>>>>>> 4ed30e5721e21fa7f0ba6b89df04b0f07f401fe3
-        </div>
-    );
+  return (
+    <div className={styles.mainHeader}>
+      <div className={styles.projectLogo}></div>
+      <div className={styles.headerText}>CHASE RUNNER</div>
+      <div className={styles.spacer}></div>
+      <Button variant="light">
+        <a href="https://passage.id/" style={{ textDecoration: "none" }}>
+          STATS
+        </a>
+      </Button>
+      <Button variant="light">
+        <a href="https://passage.id/" style={{ textDecoration: "none" }}>
+          NEW RUN
+        </a>
+      </Button>
+      <Button variant="light">
+        <a href="https://passage.id/" style={{ textDecoration: "none" }}>
+          GOALS
+        </a>
+      </Button>
+      <Dropdown>
+        <Dropdown.Toggle
+          variant="success"
+          id="dropdown-basic"
+          style={{ borderStyle: "none" }}
+        >
+          <div className={styles.imageProfile}></div>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="/logout">Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 }
 export default Banner;
