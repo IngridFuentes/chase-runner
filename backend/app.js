84674823ed17bacb399528f22d2b7ff83e040bbe
Ingrid04 <ingridfmelgar@gmail.com>
2024-11-29 21:07:48 -0800

    correct vercel url in app file backend

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.static("public"))
 app.use(cors());
 
 // const port = process.env.PORT || 3000;
-const port = "https://chase-runner-backend.vercel.app/"
+const port = "https://chase-runner-backend.vercel.app"
 
 const verifyJwt = jwt({
   secret:jwks.expressJwtSecret({
