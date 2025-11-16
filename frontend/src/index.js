fa0d633093cfd1385786df080b38c33999334b82
Ingrid04 <ingridfmelgar@gmail.com>
2025-11-16 15:39:45 -0800

    test auth router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,20 +7,20 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { UserProvider }from "./context/UserContext.jsx";
 
 ReactDOM.render(
-  <UserProvider>
-      <Router>
-          <Auth0Provider
-              domain={process.env.REACT_APP_AUTH0_DOMAIN}
-              clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-              authorizationParams={{
-                  redirect_uri: window.location.origin,
-                  audience:process.env.REACT_APP_AUTH0_AUDIENCE,
-                  scope:"openid profile email"
-              }}
-          >
-              <App />
-          </Auth0Provider>
-      </Router>,
-  </UserProvider>,
+  <Router>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+        scope: "openid profile email"
+      }}
+    >
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </Auth0Provider>
+  </Router>,
     document.getElementById('root')
 );
