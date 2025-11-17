bb000d7a12a19498abc2e57d23432c49921e63f3
Ingrid04 <ingridfmelgar@gmail.com>
2025-11-16 16:59:47 -0800

    test cors

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(express.static("public"))
   // origin: 'http://localhost:3001'
   
 const corsOptions = {
-  origin: 'https://chase-runner.vercel.app',
+  origin:[ 'https://chase-runner.vercel.app', /https:\/\/chase-runner-.*\.vercel\.app$/],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
