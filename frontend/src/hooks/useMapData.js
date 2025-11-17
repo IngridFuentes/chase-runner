c74ef19c8c04e60259b4ed3fcffde03e79be0610
Ingrid04 <ingridfmelgar@gmail.com>
2025-11-16 16:48:41 -0800

    test route fetch

--- frontend/src/hooks/useMapData.js
+++ frontend/src/hooks/useMapData.js
@@ -75,7 +75,7 @@ const useMapData = () => {
         const fetchSavedPlaces = useCallback(async () => {
           try {
             const token = await getAccessTokenSilently();
-            const response = await fetch(`${backendUrl}/user/id/runs`, {
+            const response = await fetch(`${backendUrl}/user/${encodeURIComponent(user.sub)}/runs`, {
               headers: {
                 Authorization: `Bearer ${token}`,
               },
