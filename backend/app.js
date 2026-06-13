e7143f217d1ff8146123bf8c0d8227d3dbfe72f2
Ingrid F <ingridfmelgar@gmail.com>
2026-06-12 17:07:17 -0700

    fix app.listen for vercel serverless

--- backend/app.js
+++ backend/app.js
@@ -189,7 +189,10 @@ app.use((error, req, res, next) => {
   res.status(status).send(message);
 })
 
-app.listen(3000, () => {
-    console.log("Express is running in port 3000")
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => {
+        console.log("Express is running in port 3000")
+    });
+}
+
 module.exports = app;
