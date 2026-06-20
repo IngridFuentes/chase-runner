d5e044ba2478d46585d220752391529cbf39cbd6
Ingrid F <ingridfmelgar@gmail.com>
2026-06-19 17:02:09 -0700

    update packages, and database

--- backend/config.js
+++ backend/config.js
@@ -5,11 +5,11 @@ const config = {
         port: process.env.PORT || 3000,
         corsOrigin: 'http://localhost:3001',
         database: {
-            host: 'localhost',
-            port: 5432,
-            database: 'chase_runner',
-            user: 'ingridfuentes',
-            password: 'postgres'
+            host: process.env.DATABASE_HOST,
+            port: process.env.DATABASE_PORT,
+            database: process.env.DATABASE_NAME,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD
         },
         auth0: {
             jwksUri: process.env.AUTH0_JWKS_URI,
